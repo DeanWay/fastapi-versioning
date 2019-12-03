7cff1c4892ec46ef43904829ec04afed36eef000
DeanWay <deanway93@gmail.com>
2019-12-02 19:33:52 -0500

    Bump version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fastapi_versioning",
-    version="0.1.0",
+    version="0.2.0",
     author="Dean Way",
     description="api versioning for fastapi web applications",
     long_description=long_description,
