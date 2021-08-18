cd3557c77c827ce3ab4c1914e1fb3d0d23a69bea
DeanWay <deanway93@gmail.com>
2021-08-18 11:50:02 +0200

    tests for PR-51

---
+++ tests/unit/test_root_path.py
@@ -0,0 +1,21 @@
+from typing import Any, Dict
+
+from fastapi import FastAPI, Request
+from starlette.testclient import TestClient
+from fastapi_versioning import VersionedFastAPI
+
+
+def test_root_path() -> None:
+    root_path = "/custom/root"
+    parent_app = FastAPI()
+
+    @parent_app.get("/check-root-path")
+    def check_root_path(request: Request) -> Dict[str, Any]:
+        return {"root_path": request.scope.get("root_path")}
+
+    versioned_app = VersionedFastAPI(app=parent_app, root_path=root_path)
+    test_client = TestClient(versioned_app)
+
+    response = test_client.get("/v1_0/check-root-path")
+    assert response.status_code == 200
+    assert response.json() == {"root_path": root_path + "/v1_0"}
