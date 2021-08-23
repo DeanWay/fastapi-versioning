5184d6ef9768ca2d016291865d1e894d39feed9b
DeanWay <deanway93@gmail.com>
2021-08-23 21:58:52 +0200

    Bump version from 0.9.1 to 0.10.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fastapi_versioning",
-    version="0.9.1",
+    version="0.10.0",
     author="Dean Way",
     description="api versioning for fastapi web applications",
     long_description=long_description,
